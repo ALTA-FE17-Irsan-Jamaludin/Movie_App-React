b94fbf106d7bd1a8b6d703d3e39a64bb6ae81ef7
IrsanJam <irsandjamaludin@gmail.com>
2023-12-26 13:42:52 +0700

    menambah responsivitas dan warna cerah

--- src/App.tsx
+++ src/App.tsx
@@ -94,10 +94,10 @@ export class App extends Component<{}, OutputData> {
           {this.state.showModal && this.state.selectedMovie && (
             <div className="fixed inset-0 overflow-y-auto z-50 flex items-center justify-center">
               <div className="fixed inset-0 bg-black opacity-50"></div>
-              <div className="relative flex flex-col h-[80%] justify-center items-center bg-white p-6 rounded-lg w-1/4">
+              <div className="relative flex flex-col h-[60%] sm:h-[60%] lg:h-[80%] justify-center items-center bg-white p-6 rounded-lg lg:w-1/4 w-9/12">
                 <img src={poster + this.state.selectedMovie.poster_path} className="h-[80%] w-full" height={30} alt="" />
-                <h2 className="text-xl font-bold my-4 text-white text-center py-4 px-5 w-full rounded-md bg-green-500">{this.state.selectedMovie.title}</h2>
-                <p className="text-gray-500 mb-4 text-center  tracking-tight">{this.state.selectedMovie.original_title}</p>
+                <h2 className="sm:text-xl text-base font-bold my-4 text-white text-center py-4 px-5 w-full rounded-md bg-green-500">{this.state.selectedMovie.title}</h2>
+                <p className="text-gray-500  mb-4 text-center tracking-tight">{this.state.selectedMovie.original_title}</p>
 
                 <div className="flex justify-center items-center">
                   <p className={this.state.selectedMovie.vote_average > 8 ? `text-white font-bold py-2 px-4 bg-green-400` : `text-white font-bold py-2 px-4 bg-yellow-500`}>{this.state.selectedMovie.vote_average}</p>
@@ -109,39 +109,40 @@ export class App extends Component<{}, OutputData> {
             </div>
           )}
 
-          <div className=" flex justify-center items-center h-screen w-screen">
-            <div className="left h-full flex flex-col justify-center items-center border-r-2 border-slate-400 w-2/5 shadow-md bg-zinc-900">
+          <div className=" flex sm:flex-row flex-col justify-center items-center h-screen w-screen">
+            <div className="left bg-teal-600 h-1/3 sm:h-full flex flex-col justify-center items-center border-r-2 border-slate-400  sm:w-[50%] lg:w-[35%] w-screen shadow-md">
               <span className=" font-bold text-3xl drop-shadow-md">Movies App</span>
-              <div className="search h-14 rounded-lg w-72 bg-slate-100 flex justify-between items-center  my-5 ">
+              {/* <div className="search h-14 rounded-lg w-72 bg-slate-100 flex justify-between items-center  my-5 ">
                 <div className="icon-search flex flex-col w-1/4 h-full justify-center items-center">
                   <img width="24" height="24" src="https://img.icons8.com/cotton/64/000000/search--v1.png" alt="search--v1" />
                 </div>
                 <input type="text" className="bg-slate-50 w-full h-[3.1rem] text-black rounded-lg text-center font-semibold border-b-slate-500 mr-1" />
-              </div>
+              </div> */}
 
-              <div className="grid grid-cols-1 gap-5 w-11/12 my-5 px-3">
-                <button className="shadow-md px-5 py-8 bg-slate-200 flex justify-evenly items-center opacity-95" onClick={this.favoriteButton}>
-                  <img width="64" height="64" src="https://img.icons8.com/3d-fluency/94/love-circled.png" alt="love-circled" />
-                  <span className="text-2xl font-bold text-white drop-shadow-md"> Favorite Movies</span>
+              <div className="grid grid-cols-2 sm:grid-cols-1 gap-5 w-11/12 my-5 px-3">
+                <button className=" shadow-md  px-4 py-3 lg:py-8 bg-slate-200 flex justify-center items-center opacity-95" onClick={this.playButton}>
+                  <img className="h-8 lg:h-10" src="https://img.icons8.com/3d-fluency/94/film-reel.png" alt="film-reel" />
+                  <span className="ml-2 text-2xl font-bold text-white drop-shadow-md "> Home</span>
                 </button>
-                <button className=" shadow-md px-5 py-8 bg-slate-200 flex justify-evenly items-center opacity-95" onClick={this.playButton}>
-                  <img width="64" height="64" src="https://img.icons8.com/3d-fluency/94/film-reel.png" alt="film-reel" />
-                  <span className="text-2xl font-bold text-white drop-shadow-md "> Playing Movies</span>
+                <button className="shadow-md  px-4 py-3 lg:py-8 bg-slate-200 flex justify-center items-center opacity-95" onClick={this.favoriteButton}>
+                  <img className="h-8 lg:h-10" src="https://img.icons8.com/3d-fluency/94/love-circled.png" alt="love-circled" />
+                  <span className="ml-2 text-2xl font-bold text-white drop-shadow-md"> Favorite</span>
                 </button>
               </div>
+              <hr />
             </div>
 
-            <div className="right ml-2 w-screen overflow-y-auto h-full flex flex-col justify-center items-center gap-2 ">
+            <div className="right sm:ml-2 w-screen overflow-y-auto lg:h-[92%] h-full flex flex-col justify-center items-center gap-2 ">
               {/* <h1 className="font-bold ">{this.state.play === false ? "Favorite Movies" : `Playing Movies`}</h1> */}
               {this.state.play === false ? (
-                <div className="h-full grid grid-cols-4 items-center justify-center gap-3 px-3">
+                <div className="h-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center gap-3 px-3">
                   {this.state.apiDataFavorite &&
                     this.state.apiDataFavorite.map((item: any, index: number) => {
                       return <Card key={index} name={item.title} deskripsi={item.original_title} gambar={poster + item.poster_path} rating={item.vote_average} detail={() => this.toggleModal(item)} />;
                     })}
                 </div>
               ) : (
-                <div className="h-full grid grid-cols-4 items-center justify-center gap-3 px-5">
+                <div className="h-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center gap-3 px-5">
                   {this.state.apiDataPlaying &&
                     this.state.apiDataPlaying.map((item: any, index: number) => {
                       return <Card key={index} name={item.title} deskripsi={item.original_title} gambar={poster + item.poster_path} rating={item.vote_average} detail={() => this.toggleModal(item)} />;
